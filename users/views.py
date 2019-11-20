f0c2ea42f2ede120856de1d607cb94a42a2b19f9
Marc <30148234@cityofglacol.ac.uk>
2019-11-20 14:21:37 +0000

    added templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+# def indexView(request):
+#     return render(request, 'index.html')
+#
+# def dashboardView(request):
+#     return render(request, 'dashboard.html')
+
+def registerView(request):
+    form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form':form})
